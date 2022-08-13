5ab3f3efe6944e05c397112f2de48996f872d5c4
janna98 <49-janna98@users.noreply.gitlab.cs.ut.ee>
2022-08-13 18:53:00 +0300

    Add base and form templates

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,16 +1,21 @@
 @extends('base')
 
 @section('content')
-<div>
-    Please enter your name and pick the Sectors you are currently involved in.
-        <br />
-        <br />
-        Name:
-        <input type="text">
-        <br />
-        <br />
-        Sectors:
-        <select multiple size="5">
+<form method="POST" action="/submit">
+    @csrf
+    <h3>Please enter your name and pick the sectors you are currently involved in</h3>
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input id="name" class="form-control @error('name') is-invalid @enderror"
+               type="text">
+
+        @error('name')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="sectors" class="form-label">Sectors</label>
+        <select id="sectors" class="form-select @error('name') is-invalid @enderror" multiple size="5">
             <option value="1">Manufacturing</option>
             <option value="19">&nbsp;&nbsp;&nbsp;&nbsp;Construction materials</option>
             <option value="18">&nbsp;&nbsp;&nbsp;&nbsp;Electronics and Optics</option>
@@ -91,13 +96,17 @@
             <option value="112">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Road</option>
             <option value="113">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Water</option>
         </select>
-
-        <br />
-        <br />
-        <input type="checkbox"> Agree to terms
-
-    <br />
-    <br />
-    <input type="submit" value="Save">
-</div>
+    </div>
+    <div class="mb-3 form-check">
+        <label class="form-check-label" for="agreement">Agree to terms</label>
+        <input id="agreement" class="form-check-input" type="checkbox">
+    </div>
+    <button type="submit" disabled id="submitButton" class="btn btn-primary">Save</button>
+</form>
+<script>
+    let submitButton = document.getElementById("submitButton")
+    document.getElementById('agreement').onclick = function() {
+        submitButton.disabled = !this.checked;
+    };
+</script>
 @endsection
